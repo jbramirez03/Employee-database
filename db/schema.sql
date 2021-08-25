e7f8cc0149102a731e030310a9ccafa4621b0023
temptado <jason1287712@gmail.com>
2021-08-24 20:47:13 -0400

    made change to schema file

--- db/schema.sql
+++ db/schema.sql
@@ -24,6 +24,6 @@ CREATE TABLE employees (
     last_name VARCHAR(30),
     role_id INT ,
     manager_id INT,
-    FOREIGN KEY (role_id) REFERENCES role(id) ON DELETE SET NULL,  
-    FOREIGN KEY (manager_id) REFERENCES employee(id) ON DELETE SET NULL  
+    FOREIGN KEY (role_id) REFERENCES roles(id) ON DELETE SET NULL,  
+    FOREIGN KEY (manager_id) REFERENCES employees(id) ON DELETE SET NULL  
 );
