8ac25cb366f94fc06ff57d0ea50f96e202a09809
temptado <jason1287712@gmail.com>
2021-08-24 22:56:21 -0400

    added prompt in index.js file to ask for action from user

--- index.js
+++ index.js
@@ -4,11 +4,24 @@ const actionChoices = require('./scripts/Prompts');
 
 const db = mysql.createConnection(
     {
-      host: 'localhost',
-      user: 'root',
-      password: 'jb0cme1214',
-      database: 'employees_db'
+        host: 'localhost',
+        user: 'root',
+        password: 'jb0cme1214',
+        database: 'employees_db'
     },
     console.log(`Connected to database.`)
-  );
+);
+
+inquirer
+    .prompt(actionChoices)
+    .then(answers => {
+        console.log(answers.action);
+    })
+    .catch(error => {
+        if (error) {
+            throw error;
+        } else {
+            console.log('success');
+        }
+    })
 
