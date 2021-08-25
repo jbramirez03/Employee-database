b3d49c4462963503c5b107484e6720c261bb3d4e
temptado <jason1287712@gmail.com>
2021-08-24 22:19:56 -0400

    added mysql connection to server in index.js file

--- index.js
+++ index.js
@@ -1,3 +1,12 @@
 const inquirer = require('inquirer');
 const mysql = require('mysql2');
 
+const db = mysql.createConnection(
+    {
+      host: 'localhost',
+      user: 'root',
+      password: 'jb0cme1214',
+      database: 'employees_db'
+    },
+    console.log(`Connected to database.`)
+  );
